2e97ff694113f8e529bca9dcf7519bf53b4d966b
Turki <turkiimportant@gmail.com>
2025-12-14 10:36:12 +0300

    test

--- claases.py
+++ claases.py
@@ -1,35 +1,4 @@
-class Person:
-    def __init__(self, first_name):
-        self.first_name = first_name
-
-    def greeting(self):
-            return f'Hello {self.first_name}!'
-
-p = Person('Turki')
-print(p.greeting())
-
-class Car:
-    def start(self):
-        return "Car is running"
-
-class Animal:
-    def sound(self):
-        return "animal noise"
-
-class Dog(Animal):
-    pass
-dog = Dog()
-print(dog.sound())
-
-
-class cat:
-    def sound(self):
-        return "Meow!"
-
-class Horse:
-    def sound(self):
-        return "neighing"
-
-
-for i in [cat(), Horse()]:
-    print(i.sound())
+with open("server_log.txt", "r") as rfile, open("errors_only.txt", "w") as wfile:
+    for line in rfile:
+        if "ERROR" in line:
+            wfile.write(line)
